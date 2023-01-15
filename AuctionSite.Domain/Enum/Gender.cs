68a05f2fb13562f8054fc64bd4b23de1fc1eea69
Zachary Soja <zachary3003@gmail.com>
2023-01-15 19:30:33 +0100

    feature: add domain models

---
+++ AuctionSite.Domain/Enum/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionSite.Domain.Enum
+{
+    public enum Gender
+    {
+        NotSpecified = 0,
+        Male = 1,
+        Femal = 2,
+    }
+}
---
+++ AuctionSite.Domain/Util/Implementation.cs
@@ -0,0 +1,12 @@
+﻿namespace AuctionSite.Domain.Util
+{
+    internal class Implementation : Attribute
+    {
+        public Type Interface { get; set; }
+
+        public Implementation(Type @interface)
+        {
+            Interface = @interface;
+        }
+    }
+}
